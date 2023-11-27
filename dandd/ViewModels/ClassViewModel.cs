33eb3e6d55ebf4f37993f6a93befd201cb895d2e
L30Gt <leeo12322@hotmail.com>
2023-11-27 20:00:36 -0300

    feat: Add 'Race' MVVM structure

---
+++ dandd/ViewModels/ClassViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using dandd.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dandd.Models;
+using System.Collections.ObjectModel;
+
+namespace dandd.ViewModels
+{
+    internal partial class ClassViewModel : ObservableObject
+    {
+        /*
+        private readonly ClassService _classService;
+
+        [ObservableProperty]
+        public string _Index;
+        [ObservableProperty]
+        public string _Name;
+        [ObservableProperty]
+        public string _Url;
+
+        public ObservableCollection<Class> _class;
+    */
+    }
+}
